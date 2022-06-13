4c2d1cadd990435af5ff463775b63bccd57830c3
Michał Malicki <michalmalicki@op.pl>
2022-06-13 21:04:22 +0200

    add noLog dashboard

---
+++ src/main/java/pl/coderslab/NoLogController.java
@@ -0,0 +1,18 @@
+package pl.coderslab;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class NoLogController {
+
+
+    @GetMapping("/")
+    public String hello(){
+        return "homeNoLog";
+    }
+}
